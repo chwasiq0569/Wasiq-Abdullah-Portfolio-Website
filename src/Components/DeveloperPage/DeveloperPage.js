b07fbfcaa23ab7cfafca2227131e33bd81b104e3
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-02 13:58:39 -0800

    Created Seperated COmponents

--- src/Components/DeveloperPage/DeveloperPage.js
+++ src/Components/DeveloperPage/DeveloperPage.js
@@ -3,25 +3,12 @@ import React, { useEffect } from "react";
 import DeveloperPageHero from "./DeveloperPageHero";
 import "../util/introSection.scss";
 import Footer from "./../util/Footer";
-import VsCode from "../../assets/devtools/visualStudio.png";
-import Github from "../../assets/devtools/github.png";
-import Postman from "../../assets/devtools/icons8-postman-api-128.png";
-import Npm from "../../assets/devtools/npm.png";
-import WebPack from "../../assets/devtools/icons8-webpack-512.png";
+
 import TiltingEyes from "./../util/TiltingEyes/TiltingEyes";
 //projects
 import CovidTrackerImg from "../../assets/DeveloperProj/CovidTracker2.png";
 import MovieStreamApp from "../../assets/DeveloperProj/MovieStreamApp.jpg";
-import HTMLIcon from "../../assets/DeveloperLanguages/html5.png";
-import CSSIcon from "../../assets/DeveloperLanguages/css.png";
-import WordpressIcon from "../../assets/DeveloperLanguages/wordpress.png";
-import JSIcon from "../../assets/DeveloperLanguages/javascript.png";
-import BootstrapIcon from "../../assets/DeveloperLanguages/bootstrap.png";
-import ReactIcon from "../../assets/DeveloperLanguages/react.png";
-import NodeJSIcon from "../../assets/DeveloperLanguages/nodejs.png";
-import JavaIcon from "../../assets/DeveloperLanguages/java.png";
-import SassIcon from "../../assets/DeveloperLanguages/sass.png";
-import ReduxIcon from "../../assets/DeveloperLanguages/redux.png";
+
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import { motion } from "framer-motion";
@@ -29,178 +16,13 @@ import IntroSection from "./../util/IntroSection";
 import CartImg from "../../assets/DesignProj/icons8-fast-cart-64.png";
 import ChatRoomIcon from "../../assets/DeveloperProj/icons8-chat-room-100.png";
 import IconContainer from "./IconContainer";
+import { row1, row2, row3, toolsIcons } from "./IconsData";
 
 const DeveloperPage = ({ page, text, pageData }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
     document.body.style.overflowY = "scroll";
   }, []);
-
-  const row1 = [
-    {
-      className: "iconContainer",
-      data: "zoom-in",
-      delay: "150",
-      duration: "700",
-      text: "HTML5",
-      Icon: HTMLIcon,
-      innerClass: "tooltip",
-
-      altText: "HTML Icon",
-    },
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "200",
-      duration: "700",
-      text: "CSS3",
-      Icon: CSSIcon,
-      innerClass: "tooltip",
-
-      altText: "CSS Icon",
-    },
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "250",
-      duration: "700",
-      text: "Javascript",
-      Icon: JSIcon,
-      innerClass: "tooltip",
-
-      altText: "Javascript Icon",
-    },
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "300",
-      duration: "700",
-      text: "Wordpress",
-      Icon: WordpressIcon,
-      innerClass: "tooltip",
-
-      altText: "Wordpress Icon",
-    },
-  ];
-  const row2 = [
-    {
-      className: "iconContainer",
-      data: "zoom-in",
-      delay: "350",
-      duration: "700",
-      text: "Bootstrap",
-      Icon: BootstrapIcon,
-      innerClass: "tooltip",
-      altText: "Bootstrap Icon",
-    },
-    {
-      className: "iconContainer middleBigIcon",
-      data: "zoom-in",
-      delay: "400",
-      duration: "700",
-      text: "React-JS",
-      Icon: ReactIcon,
-      innerClass: "react-tooltip",
-      altText: "React Icon",
-    },
-  ];
-
-  const row3 = [
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "500",
-      duration: "700",
-      text: "NodeJS",
-      Icon: NodeJSIcon,
-      innerClass: "tooltip",
-
-      altText: "NodeJS Icon",
-    },
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "550",
-      duration: "700",
-      text: "Java",
-      Icon: JavaIcon,
-      innerClass: "tooltip",
-
-      altText: "Java Icon",
-    },
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "600",
-      duration: "700",
-      text: "Sass",
-      Icon: SassIcon,
-      innerClass: "tooltip",
-
-      altText: "Sass Icon",
-    },
-    {
-      className: "iconContainer",
-
-      data: "zoom-in",
-      delay: "650",
-      duration: "700",
-      text: "Redux",
-      Icon: ReduxIcon,
-      innerClass: "tooltip",
-
-      altText: "Redux Icon",
-    },
-  ];
-
-  const toolsIcons = [
-    {
-      className: "iconContainer",
-      innerClass: "tooltip",
-      data: "zoom-in",
-      delay: "600",
-      duration: "1000",
-      text: "VS-CODE",
-      Icon: VsCode,
-      altText: "VS-CODE Icon",
-    },
-    {
-      className: "iconContainer",
-      innerClass: "tooltip",
-      data: "zoom-in",
-      delay: "1200",
-      duration: "1000",
-      text: "Webpack",
-      Icon: WebPack,
-      altText: "WebPack Icon",
-    },
-    {
-      className: "iconContainer",
-      innerClass: "tooltip",
-      data: "zoom-in",
-      delay: "1000",
-      duration: "1000",
-      text: "Postman",
-      Icon: Postman,
-      altText: "Postman Icon",
-    },
-    {
-      className: "iconContainer",
-      innerClass: "tooltip",
-      data: "zoom-in",
-      delay: "1400",
-      duration: "1000",
-      text: "NPM",
-      Icon: Npm,
-      altText: "NPM Icon",
-    },
-  ];
 
   return (
     <motion.div
---
+++ src/Components/DeveloperPage/IconsData.js
@@ -0,0 +1,180 @@
+import HTMLIcon from "../../assets/DeveloperLanguages/html5.png";
+import CSSIcon from "../../assets/DeveloperLanguages/css.png";
+import WordpressIcon from "../../assets/DeveloperLanguages/wordpress.png";
+import JSIcon from "../../assets/DeveloperLanguages/javascript.png";
+import BootstrapIcon from "../../assets/DeveloperLanguages/bootstrap.png";
+import ReactIcon from "../../assets/DeveloperLanguages/react.png";
+import NodeJSIcon from "../../assets/DeveloperLanguages/nodejs.png";
+import JavaIcon from "../../assets/DeveloperLanguages/java.png";
+import SassIcon from "../../assets/DeveloperLanguages/sass.png";
+import ReduxIcon from "../../assets/DeveloperLanguages/redux.png";
+import VsCode from "../../assets/devtools/visualStudio.png";
+import Github from "../../assets/devtools/github.png";
+import Postman from "../../assets/devtools/icons8-postman-api-128.png";
+import Npm from "../../assets/devtools/npm.png";
+import WebPack from "../../assets/devtools/icons8-webpack-512.png";
+export const row1 = [
+  {
+    className: "iconContainer",
+    data: "zoom-in",
+    delay: "150",
+    duration: "700",
+    text: "HTML5",
+    Icon: HTMLIcon,
+    innerClass: "tooltip",
+
+    altText: "HTML Icon",
+  },
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "200",
+    duration: "700",
+    text: "CSS3",
+    Icon: CSSIcon,
+    innerClass: "tooltip",
+
+    altText: "CSS Icon",
+  },
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "250",
+    duration: "700",
+    text: "Javascript",
+    Icon: JSIcon,
+    innerClass: "tooltip",
+
+    altText: "Javascript Icon",
+  },
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "300",
+    duration: "700",
+    text: "Wordpress",
+    Icon: WordpressIcon,
+    innerClass: "tooltip",
+
+    altText: "Wordpress Icon",
+  },
+];
+export const row2 = [
+  {
+    className: "iconContainer",
+    data: "zoom-in",
+    delay: "350",
+    duration: "700",
+    text: "Bootstrap",
+    Icon: BootstrapIcon,
+    innerClass: "tooltip",
+    altText: "Bootstrap Icon",
+  },
+  {
+    className: "iconContainer middleBigIcon",
+    data: "zoom-in",
+    delay: "400",
+    duration: "700",
+    text: "React-JS",
+    Icon: ReactIcon,
+    innerClass: "react-tooltip",
+    altText: "React Icon",
+  },
+];
+
+export const row3 = [
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "500",
+    duration: "700",
+    text: "NodeJS",
+    Icon: NodeJSIcon,
+    innerClass: "tooltip",
+
+    altText: "NodeJS Icon",
+  },
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "550",
+    duration: "700",
+    text: "Java",
+    Icon: JavaIcon,
+    innerClass: "tooltip",
+
+    altText: "Java Icon",
+  },
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "600",
+    duration: "700",
+    text: "Sass",
+    Icon: SassIcon,
+    innerClass: "tooltip",
+
+    altText: "Sass Icon",
+  },
+  {
+    className: "iconContainer",
+
+    data: "zoom-in",
+    delay: "650",
+    duration: "700",
+    text: "Redux",
+    Icon: ReduxIcon,
+    innerClass: "tooltip",
+
+    altText: "Redux Icon",
+  },
+];
+
+export const toolsIcons = [
+  {
+    className: "iconContainer",
+    innerClass: "tooltip",
+    data: "zoom-in",
+    delay: "600",
+    duration: "1000",
+    text: "VS-CODE",
+    Icon: VsCode,
+    altText: "VS-CODE Icon",
+  },
+  {
+    className: "iconContainer",
+    innerClass: "tooltip",
+    data: "zoom-in",
+    delay: "1200",
+    duration: "1000",
+    text: "Webpack",
+    Icon: WebPack,
+    altText: "WebPack Icon",
+  },
+  {
+    className: "iconContainer",
+    innerClass: "tooltip",
+    data: "zoom-in",
+    delay: "1000",
+    duration: "1000",
+    text: "Postman",
+    Icon: Postman,
+    altText: "Postman Icon",
+  },
+  {
+    className: "iconContainer",
+    innerClass: "tooltip",
+    data: "zoom-in",
+    delay: "1400",
+    duration: "1000",
+    text: "NPM",
+    Icon: Npm,
+    altText: "NPM Icon",
+  },
+];
