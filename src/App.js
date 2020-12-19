6fe2631af17b21e399a3a991aafd397473256211
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-19 09:38:06 -0800

    Fixed Typo

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AboutPage from "./Components/AboutPage/AboutPage";
 import HeroComp from "./Components/util/HeroComp";
 import Cursors from "./Components/Cursor/Cursors";
 import Cursor from "./Components/Cursor/CursorCursor";
+import { pageDataArr } from "./Components/util/utils";
 function App(props) {
   const location = useLocation();
   window.onload = () => {
@@ -36,36 +37,7 @@ function App(props) {
     document.body.style.backgroundColor = " #1544cd";
   }
 
-  const [pageData, setPageData] = useState([
-    {
-      introTitle: "INTRO",
-      introHead:
-        " I am a Designer who codes. I strive to create simple, original and impactful User Experiences.",
-      introDesc:
-        " I mainly use Adobe Tool to design UI/UX. And then a Javascript Library known as Reactjs to implement it. I also use many other tools to achieve the main Goal and to create better Front End Experience of my App.",
-    },
-    {
-      introTitle: "ABOUT ME",
-      introHead:
-        "Hey! My name is Wasiq Abdullah and I'm Front-End Designer & Developer who convert your ideas into Reality.",
-      introDesc:
-        "The main area of my expertise is Front-End Design & Development (client side of web). I can design and develop slick, interactive, Responsive and Performance Efficient Web Apps. I also have experience of creating websites on CMS like Wordpress, Wix Website Builder and others.",
-    },
-    {
-      introTitle: "DESIGN",
-      introHead:
-        "I love to design experiences that not only complete the functional value but also loved by people.",
-      introDesc:
-        "I am a UI designer. Currently working on freelance projects and updating my portfolio. I believe great design is defined by the experience it creates for the User combined with flawless execution. I'm a designer with a keen eye for creating engaging UI, bringing products to life. ",
-    },
-    {
-      introTitle: "DEVELOP",
-      introHead:
-        "I'm an ingenious web developer oriented to the frontend, I lead and design products that people love.",
-      introDesc:
-        " I rely on the most outstanding open source libraries React & Redux being these my tools to be able to solve any challenge posed in my work life. I bring unique prespective of designer & developer. This means my design takes into account users as well as developer constraints allowing me to take on digital projects from multiple standpoints. ",
-    },
-  ]);
+  const [pageData, setPageData] = useState(pageDataArr);
   return (
     <>
       <Cursors />
--- src/Components/util/utils.js
+++ src/Components/util/utils.js
@@ -5,3 +5,34 @@ export const changeRouteOne = (route, wait, props) => {
   //before pushing it will wait 600ms
   setTimeout(() => props.history.push(route), wait);
 };
+
+export const pageDataArr = [
+  {
+    introTitle: "INTRO",
+    introHead:
+      " I am a Designer who codes. I strive to create simple, original and impactful User Experiences.",
+    introDesc:
+      " I mainly use Adobe Tool to design UI/UX. And then a Javascript Library known as Reactjs to implement it. I also use many other tools to achieve the main Goal and to create better Front End Experience of my App.",
+  },
+  {
+    introTitle: "ABOUT ME",
+    introHead:
+      "Hey! My name is Wasiq Abdullah and I'm Front-End Designer & Developer who convert your ideas into Reality.",
+    introDesc:
+      "The main area of my expertise is Front-End Design & Development (client side of web). I can design and develop slick, interactive, Responsive and Performance Efficient Web Apps. I have one year experience of creating websites on CMS like Wordpress.",
+  },
+  {
+    introTitle: "DESIGN",
+    introHead:
+      "I love to design experiences that not only complete the functional value but also loved by people.",
+    introDesc:
+      "I am a UI designer. Currently working on freelance projects and updating my portfolio. I believe great design is defined by the experience it creates for the User combined with flawless execution. I'm a designer with a keen eye for creating engaging UI, bringing products to life. ",
+  },
+  {
+    introTitle: "DEVELOP",
+    introHead:
+      "I'm an ingenious web developer oriented to the frontend, I lead and design products that people love.",
+    introDesc:
+      " I rely on the most outstanding open source libraries React & Redux being these my tools to be able to solve any challenge posed in my work life. I bring unique prespective of designer & developer. This means my design takes into account users as well as developer constraints allowing me to take on digital projects from multiple standpoints. ",
+  },
+];
